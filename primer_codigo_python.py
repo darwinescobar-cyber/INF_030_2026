1ec7729d9a59f7d40f8f30f896ecb1b9f02f88da
darwinescobar-cyber <darwin.escobar@ucb.edu.bo>
2026-01-08 12:26:05 -0400

    python

---
+++ primer_codigo_python.py
@@ -0,0 +1,18 @@
+print("hola, mundo")
+x = 5
+y = 10
+resultado = x + y 
+print("hola, Alex El resultado es: ")
+print (resultado)
+
+print("---------------------------------")
+
+print ("Calcular el area de un rectangulo")
+base = 10 
+altura = 57
+area_rectangulo = base * altura 
+print (area_rectangulo)
+
+print("---------------------------------")
+
+print(7*8)
